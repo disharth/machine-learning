d6f83b38b872e9b24d1dd79d370502cba4eb8484
Pramod Shukla <pramod.shukla@mendix.com>
2018-09-15 22:25:13 +0200

    simple NN with keras.

---
+++ time-series/keras-nn.py
@@ -0,0 +1,26 @@
+# Keras imports
+from keras.models import Sequential
+from keras.layers import Dense
+
+# The numpy imports
+import numpy
+
+dataset = numpy.loadtxt('pima-indians-diabetes.csv' , delimiter=',')
+
+X = dataset[:,0:8]
+Y = dataset[:,8]
+
+model = Sequential()
+model.add(Dense(12 , input_dim=8,activation='relu'))
+model.add(Dense(8 , activation='relu'))
+model.add(Dense(1,activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy' , optimizer='adam',metrics=['accuracy'])
+
+model.fit(X,Y,epochs=150, batch_size=10)
+
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
