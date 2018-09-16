eb33b0d70662c1f7db773de919fcfb05519956f3
Pramod Shukla <pramod.shukla@mendix.com>
2018-09-16 08:48:07 +0200

    more comments

--- time-series/keras-nn.py
+++ time-series/keras-nn.py
@@ -7,19 +7,25 @@ import numpy
 
 dataset = numpy.loadtxt('pima-indians-diabetes.csv' , delimiter=',')
 
+# input variables
 X = dataset[:,0:8]
+
+# output variable
 Y = dataset[:,8]
 
+# create network architecture
 model = Sequential()
 model.add(Dense(12 , input_dim=8,activation='relu'))
 model.add(Dense(8 , activation='relu'))
 model.add(Dense(1,activation='sigmoid'))
 
+# create computation graph
 model.compile(loss='binary_crossentropy' , optimizer='adam',metrics=['accuracy'])
 
+# run the graph
 model.fit(X,Y,epochs=150, batch_size=10)
 
-# evaluate the model
+# evaluate the model on training set
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
